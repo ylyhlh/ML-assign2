3c9848045b8ba9a429e51513aaa09cfb86bd6a12
hl1283 <hl1283@nyu.edu>
2012-12-03 14:54:14 -0500

    sub gradiputtest

--- models.lua
+++ models.lua
@@ -18,9 +18,13 @@ end
 
 function modRBF(inputs,hunits,outputs,initw)
 local mlp = nn.Sequential()
-mlp:add( nn.RBFA(inputs,hunits) )
+----[[
+mlp:add( nn.RBF(inputs,hunits) )
 mlp:add( nn.MulPos(hunits,initw))
 mlp:add( nn.NegExp() ) 
+--]]
+--mlp:add( nn.Linear(inputs,hunits) )
+mlp:add( nn.Tanh() ) 
 mlp:add( nn.Linear(hunits,outputs) )
 mlp:add( nn.LogSoftMax() )
 return mlp
@@ -42,17 +46,27 @@ end
 
 return error
 end
---[[
+----[[
 function test_GrandInput(module,input)
-   for i=1,sin:nElement() do      
-      sin[i] = sin[i] - small
-      outa:copy(module:forward(input))
-      sin[i] = sin[i] + 2*small
-      outb:copy(module:forward(input))
-      sin[i] = sin[i] - small
-
-      outb:add(-1,outa):div(2*small)
-      jacobian:select(1,i):copy(outb)
-   end  
+ local small=1E-6
+ local out_size=module:forward(input):nElement()
+ local gradInput0= torch.Tensor():resize(input:nElement(),module:forward(input):nElement())
+ local gradInput1= torch.Tensor():resize(input:nElement(),module:forward(input):nElement())
+   for i=1,input:nElement() do      
+      input[i] = input[i] + small
+      local out0=module:forward(input):clone()
+      input[i] = input[i] - 2*small
+      local out1=module:forward(input):clone()
+      input[i] = input[i] + small
+      out0:add(-1,out1):div(2*small)
+      gradInput1:select(1,i):copy(out0)
+   end 
+   for i=1,out_size do     
+      local out=torch.zeros(out_size)
+      out[i]=1
+      local ingrad=module:backward(input,out):clone()
+      gradInput0:select(2,i):copy(ingrad)
+   end
+   print('The GradInput error:'..torch.norm(gradInput0-gradInput1)) 
 end
-]]--
+--]]--
